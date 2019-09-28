05fa5d43d9950804c6d29a0f071a12567ca2ed0d
Johnny Willemsen <jwillemsen@remedy.nl>
2019-09-28 09:30:04 +0200

    Updated TAOX11 version to 1.8.0

--- tao/x11/versionx11.h
+++ tao/x11/versionx11.h
@@ -8,6 +8,6 @@
  * Chamber of commerce Rotterdam nr.276339, The Netherlands
  * -------------------------------------------------------------------*/
 #define TAOX11_MAJOR_VERSION 1
-#define TAOX11_MINOR_VERSION 7
-#define TAOX11_MICRO_VERSION 1
-#define TAOX11_VERSION "1.7.1"
+#define TAOX11_MINOR_VERSION 8
+#define TAOX11_MICRO_VERSION 0
+#define TAOX11_VERSION "1.8.0"
