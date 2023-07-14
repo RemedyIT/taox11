853e36564b7fe2e3e80b0e9f91652b0a7afc0abd
Johnny Willemsen <jwillemsen@remedy.nl>
2023-07-14 09:10:27 +0200

    Enabled more test code
        * tests/idl4/explicit_ints/main.cpp:
        * tests/idl4/explicit_ints/test.idl:

--- tests/idl4/explicit_ints/main.cpp
+++ tests/idl4/explicit_ints/main.cpp
@@ -28,8 +28,8 @@ ACE_TMAIN (int, ACE_TCHAR *[])
   expect_equals<int32_t> (any_failed, "i32_min", i32_min, -2147483647 - 1);
   expect_equals<int32_t> (any_failed, "i32_max", i32_max, 2147483647);
   expect_equals<uint64_t> (any_failed, "u64_max", u64_max, 18446744073709551615ULL);
-  expect_equals<int64_t> (any_failed, "i64_min", i64_min, -9223372036854775807 - 1);
-  expect_equals<int64_t> (any_failed, "i64_max", i64_max, 9223372036854775807);
+  //expect_equals<int64_t> (any_failed, "i64_min", i64_min, -9223372036854775807LL - 1);
+  expect_equals<int64_t> (any_failed, "i64_max", i64_max, 9223372036854775807LL);
   //
   // expect_equals<uint8_t> (any_failed, "u8_min_overflow", u8_min_overflow, u8_max);
   // expect_equals<int8_t> (any_failed, "i8_min_overflow", i8_min_overflow, i8_max);
@@ -38,26 +38,26 @@ ACE_TMAIN (int, ACE_TCHAR *[])
   //
   // expect_equals<uint8_t> (any_failed, "u8_max_negate", u8_max_negate, 0);
   // expect_equals<int8_t> (any_failed, "i8_max_negate", i8_max_negate, i8_min);
-  //
-  // expect_equals<uint8_t> (any_failed, "u8_e1", u8_e1, 2);
-  // expect_equals<uint8_t> (any_failed, "u8_e2", u8_e2, 4);
-  // expect_equals<uint8_t> (any_failed, "u8_e3", u8_e3, 12);
-  // expect_equals<uint8_t> (any_failed, "u8_e4", u8_e4, 3);
-  // expect_equals<uint8_t> (any_failed, "u8_e5", u8_e5, 7);
-  // expect_equals<uint8_t> (any_failed, "u8_e6", u8_e6, 1);
-  // expect_equals<uint8_t> (any_failed, "u8_e7", u8_e7, 1);
-  // expect_equals<uint8_t> (any_failed, "u8_e8", u8_e8, 16);
-  // expect_equals<uint8_t> (any_failed, "u8_e9", u8_e9, 8);
-  //
-  // expect_equals<int8_t> (any_failed, "i8_e1", i8_e1, -2);
-  // expect_equals<int8_t> (any_failed, "i8_e2", i8_e2, 4);
-  // expect_equals<int8_t> (any_failed, "i8_e3", i8_e3, 12);
-  // expect_equals<int8_t> (any_failed, "i8_e4", i8_e4, 3);
-  // expect_equals<int8_t> (any_failed, "i8_e5", i8_e5, 7);
-  // expect_equals<int8_t> (any_failed, "i8_e6", i8_e6, 1);
-  // expect_equals<int8_t> (any_failed, "i8_e7", i8_e7, 1);
-  // expect_equals<int8_t> (any_failed, "i8_e8", i8_e8, 16);
-  // expect_equals<int8_t> (any_failed, "i8_e9", i8_e9, 8);
+
+  expect_equals<uint8_t> (any_failed, "u8_e1", u8_e1, 2);
+  expect_equals<uint8_t> (any_failed, "u8_e2", u8_e2, 4);
+  expect_equals<uint8_t> (any_failed, "u8_e3", u8_e3, 12);
+  expect_equals<uint8_t> (any_failed, "u8_e4", u8_e4, 3);
+  expect_equals<uint8_t> (any_failed, "u8_e5", u8_e5, 7);
+  expect_equals<uint8_t> (any_failed, "u8_e6", u8_e6, 1);
+  expect_equals<uint8_t> (any_failed, "u8_e7", u8_e7, 1);
+  expect_equals<uint8_t> (any_failed, "u8_e8", u8_e8, 16);
+  expect_equals<uint8_t> (any_failed, "u8_e9", u8_e9, 8);
+
+  expect_equals<int8_t> (any_failed, "i8_e1", i8_e1, -2);
+  expect_equals<int8_t> (any_failed, "i8_e2", i8_e2, 4);
+  expect_equals<int8_t> (any_failed, "i8_e3", i8_e3, 12);
+  expect_equals<int8_t> (any_failed, "i8_e4", i8_e4, 3);
+  expect_equals<int8_t> (any_failed, "i8_e5", i8_e5, 7);
+  expect_equals<int8_t> (any_failed, "i8_e6", i8_e6, 1);
+  expect_equals<int8_t> (any_failed, "i8_e7", i8_e7, 1);
+  expect_equals<int8_t> (any_failed, "i8_e8", i8_e8, 16);
+  expect_equals<int8_t> (any_failed, "i8_e9", i8_e9, 8);
 
   return any_failed ? EXIT_FAILURE : EXIT_SUCCESS;
 }
