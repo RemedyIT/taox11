90f767d69d240a4d843804ded6c502fb498d774c
Johnny Willemsen <jwillemsen@remedy.nl>
2023-08-22 11:04:57 +0200

    Generate and test struct ostream insertion with inheritance
        * ridlbe/c++11/templates/cli/hdr/struct_idl_traits_def.erb:
        * tests/idl4/struct_inheritance/client.cpp:
        * tests/idl4/struct_inheritance/test.mpc:

--- tests/idl4/struct_inheritance/client.cpp
+++ tests/idl4/struct_inheritance/client.cpp
@@ -8,15 +8,26 @@
  */
 
 #include "testC.h"
+#include "testlib/taox11_testlog.h"
 
 int main (int /*argc*/, char* /*argv*/[])
 {
   // Just compilation test
-  EmptyStruct es;
-  X11_UNUSED_ARG(es);
   Point2D p2d;
-  X11_UNUSED_ARG(p2d);
+  TAOX11_TEST_DEBUG << "Point2D: " << p2d << std::endl;
   Point3D p3d;
-  X11_UNUSED_ARG(p3d);
+  TAOX11_TEST_DEBUG << "Point3D: " << p3d << std::endl;
+  Point4D p4d;
+  TAOX11_TEST_DEBUG << "Point4D: " << p4d << std::endl;
+  EmptyStruct es;
+  TAOX11_TEST_DEBUG << "EmptyStruct: " << es << std::endl;
+  DeriveEmpty de;
+  TAOX11_TEST_DEBUG << "DeriveEmpty: " << de << std::endl;
+  DeriveEmptyWithMember dewm;
+  TAOX11_TEST_DEBUG << "DeriveEmptyWithMember: " << dewm << std::endl;
+  EmptyFromDerived efd;
+  TAOX11_TEST_DEBUG << "EmptyFromDerived: " << efd << std::endl;
+  EmptyFromDerivedDerived efdd;
+  TAOX11_TEST_DEBUG << "EmptyFromDerivedDerived: " << efdd << std::endl;
   return 0;
 }
