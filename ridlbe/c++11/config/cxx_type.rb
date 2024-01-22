04054c97aa01d438a43ecc5322b52fd8cdb66a3e
Johnny Willemsen <jwillemsen@remedy.nl>
2024-01-22 11:38:06 +0100

    Implement default_literal for enum
        * ridlbe/c++11/config/cxx_type.rb:
        * ridlbe/c++11/visitors/enum.rb:

--- ridlbe/c++11/config/cxx_type.rb
+++ ridlbe/c++11/config/cxx_type.rb
@@ -862,21 +862,13 @@ module IDL
       end
 
       def value_initializer
-        # When we have an annotation directly applied to this node we are using it
-        # unless node.annotations[:default].first.nil?
-        #   "{#{node.annotations[:default].first.fields[:value]}}"
-        # else
-        #   # Check whether it is a typedef, if so, we need to see if there is an annotation applied to the typedef (or its typedef)
-        #   res_idl_type = _idltype
-        #   while res_idl_type.is_a?(IDL::Type::ScopedName)
-        #     unless res_idl_type.node.annotations[:default].first.nil?
-        #       return "{#{res_idl_type.node.annotations[:default].first.fields[:value]}}"
-        #     end
-        #     res_idl_type = res_idl_type.node.idltype
-        #   end
-        #   _resolved_idltype.zero_initializer
-        # end
-        return "FOO"
+        res = '{}'
+        node.enumerators.each do |e|
+          unless e.annotations[:default_literal].first.nil?
+            res = '{' + cxx_type + '::' + e.scoped_cxxname + '}'
+          end
+        end
+        return res
       end
     end
 
