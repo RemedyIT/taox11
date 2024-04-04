0a8b1ce10dfd6ee0ac469d616af47fdac2349cf0
Johnny Willemsen <jwillemsen@remedy.nl>
2024-04-04 13:30:35 +0200

    Add taox11 client
        * tests/proxysource/taox11client.cpp:
          Added.
    
        * tests/proxysource/proxysource.mpc:

---
+++ tests/proxysource/taox11client.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file    client.cpp
+ * @author  Johnny Willemsen
+ *
+ * @brief   C++11 client application which uses IDL defines types
+ *          and TAOX11
+ *
+ * @copyright Copyright (c) Remedy IT Expertise BV
+ */
+
+#include "testC.h"
+
+// Check for ACE define
+
+int
+main(int, char* [])
+{
+  int result {};
+
+  try
+  {
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << "exception caught: " << e.what() << std::endl;
+    return 1;
+  }
+  return result;
+}
