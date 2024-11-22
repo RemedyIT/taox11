cb60e5528d83e59f97a75acca4350847a6bf5364
Johnny Willemsen <jwillemsen@remedy.nl>
2024-11-22 13:39:51 +0100

    Extended ostream insertion logging
        * ridlbe/c++11/templates/cli/hdr/bitset_idl_traits.erb:
        * tests/idl4/bitset/client.cpp:

--- tests/idl4/bitset/client.cpp
+++ tests/idl4/bitset/client.cpp
@@ -66,6 +66,60 @@ int main (int argc, char* argv[])
         }
       TAOX11_TEST_DEBUG << "Received MyBitset1 sret1: " << sret1 << " sinout1: " << sinout1 << " sout1: " << sout1 << std::endl;
 
+      MyBitset2 sin2 {};
+      MyBitset2 sinout2 {};
+      MyBitset2 sout2 {};
+
+      TAOX11_TEST_DEBUG << "Sending MyBitset2 sin: " << sin2 << " sinout: " << sinout2 << std::endl;
+      MyBitset2 const sret2 = foo->test_mybitset2 (sin2, sinout2, sout2);
+      if (sout2 != sin2)
+        {
+          TAOX11_TEST_ERROR << "ERROR: sout2<" << sout2 << "> != sin2<" << sin2 << ">" << std::endl;
+          ++error_count;
+        }
+      if (sinout2 != sin2)
+        {
+          TAOX11_TEST_ERROR << "ERROR: sinout2<" << sinout2 << "> != sin<" << sin2 << ">" << std::endl;
+          ++error_count;
+        }
+      TAOX11_TEST_DEBUG << "Received MyBitset2 sret2: " << sret2 << " sinout2: " << sinout2 << " sout2: " << sout2 << std::endl;
+
+      MyBitset3 sin3 {};
+      MyBitset3 sinout3 {};
+      MyBitset3 sout3 {};
+
+      TAOX11_TEST_DEBUG << "Sending MyBitset3 sin: " << sin3 << " sinout: " << sinout3 << std::endl;
+      MyBitset3 const sret3 = foo->test_mybitset3 (sin3, sinout3, sout3);
+      if (sout3 != sin3)
+        {
+          TAOX11_TEST_ERROR << "ERROR: sout3<" << sout3 << "> != sin3<" << sin3 << ">" << std::endl;
+          ++error_count;
+        }
+      if (sinout3 != sin3)
+        {
+          TAOX11_TEST_ERROR << "ERROR: sinout3<" << sinout3 << "> != sin<" << sin3 << ">" << std::endl;
+          ++error_count;
+        }
+      TAOX11_TEST_DEBUG << "Received MyBitset3 sret3: " << sret3 << " sinout3: " << sinout3 << " sout3: " << sout3 << std::endl;
+
+      MyBitset4 sin4 {};
+      MyBitset4 sinout4 {};
+      MyBitset4 sout4 {};
+
+      TAOX11_TEST_DEBUG << "Sending MyBitset4 sin: " << sin4 << " sinout: " << sinout4 << std::endl;
+      MyBitset4 const sret4 = foo->test_mybitset4 (sin4, sinout4, sout4);
+      if (sout4 != sin4)
+        {
+          TAOX11_TEST_ERROR << "ERROR: sout4<" << sout4 << "> != sin4<" << sin4 << ">" << std::endl;
+          ++error_count;
+        }
+      if (sinout4 != sin4)
+        {
+          TAOX11_TEST_ERROR << "ERROR: sinout4<" << sinout4 << "> != sin<" << sin4 << ">" << std::endl;
+          ++error_count;
+        }
+      TAOX11_TEST_DEBUG << "Received MyBitset4 sret4: " << sret4 << " sinout4: " << sinout4 << " sout4: " << sout4 << std::endl;
+
       TAOX11_TEST_DEBUG << "shutting down..." << std::endl;
       foo->shutdown ();
       _orb->destroy ();
